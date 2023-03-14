db02d8107d57174542b8f5502795b01dc86dcc21
yk3372 <yk3372@gmail.com>
2023-03-14 21:07:01 +0800

    bugfix
    Signed-off-by: yk3372 <yk3372@gmail.com>

--- lib/src/components/f_menu/src/f_menu_group.dart
+++ lib/src/components/f_menu/src/f_menu_group.dart
@@ -4,8 +4,33 @@ import 'f_base_menu.dart';
 class FMenuGroup extends FBaseMenu {
   final List<FBaseMenu> children;
   final String groupTitle;
+
   FMenuGroup({
     this.children = const [],
     this.groupTitle = '-',
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! FMenuGroup) {
+      return false;
+    }
+    if (groupTitle != other.groupTitle) {
+      return false;
+    }
+    if (children.length != other.children.length) {
+      return false;
+    }
+    for (int i = 0; i < children.length; i++) {
+      if (children[i] != other.children[i]) {
+        return false; //直接跳出并返回即可
+      }
+    }
+    return true;
+  }
+
+  @override
+  int get hashCode {children.hashCode
+    return groupTitle.hashCode;
+  }
 }
